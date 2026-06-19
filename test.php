85787967e8c030b9eb37bd7bc02f3e0a5028582e
WorkOfStan <rejthar@stanislavrejthar.com>
2026-06-19 23:00:20 +0200

    docs: add explanation to SLOWSTEP

--- test.php
+++ test.php
@@ -22,7 +22,7 @@ $conf = [
     Logger::CONF_LOGGING_LEVEL => 0, // start with logging almost nothing for purpose of test looping
     Logger::CONF_LOGGING_LEVEL_PAGE_SPEED => 5,
     Logger::CONF_LOG_MONTHLY_ROTATION => true,
-    Logger::CONF_LOG_PROFILING_STEP => 0.00048,
+    Logger::CONF_LOG_PROFILING_STEP => 0.00048, // very short to actually log all lines as SLOWSTEP
     Logger::CONF_MAIL_FOR_ADMIN_ENABLED => false,
 ];
 $logger = new Logger($conf);
